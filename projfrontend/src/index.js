be377fb04a1fc7f6f396ee8cf5c13728e8560167
Naman <namansngh64@gmail.com>
2022-06-15 22:23:19 +0530

    commit test

--- projfrontend/src/index.js
+++ projfrontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
+
 // import "bootstrap/dist/css/bootstrap.min.css";
 // import "bootstrap/dist/js/bootstrap.bundle.min";
 
